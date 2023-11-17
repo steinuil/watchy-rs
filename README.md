92d25b61674f6bcbb8654a6cd4f39c219ac352cb
steinuil <steenuil.owl@gmail.com>
2023-11-17 12:39:56 +0100

    wip

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 https://watchy.sqfmi.com/docs/hardware
 
 - Microcontroller ESP32-PICO-D4: esp-idf-hal ?
-- E-Paper Display GDEH0154D67: https://docs.rs/gdeh0154d67/latest/gdeh0154d67/
+- E-Paper Display GDEH0154D67
+  - https://docs.rs/gdeh0154d67/latest/gdeh0154d67/
+  - https://gitlab.com/CasalI/gdeh0154d67
+  - https://github.com/ZinggJM/GxEPD/blob/master/src/GxGDEH0154D67/GxGDEH0154D67.cpp
 - Real time clock PCF8563: https://docs.rs/pcf8563/latest/pcf8563/
 - 3-axis accelerometer BMA423: https://docs.rs/bma423/latest/bma423/
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
 #![no_std]
 #![no_main]
+#![feature(type_alias_impl_trait)]
 
+use embassy_executor::Executor;
+use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
 use esp_println::println;
-use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc};
+use hal::{
+    clock::ClockControl, embassy, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc,
+};
+use static_cell::StaticCell;
+
+static EXECUTOR: StaticCell<Executor> = StaticCell::new();
+
+#[embassy_executor::task]
+async fn run1() {
+    loop {
+        println!("ayy");
+        Timer::after(Duration::from_millis(1000)).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn run2() {
+    loop {
+        println!("lmao");
+        Timer::after(Duration::from_millis(2000)).await;
+    }
+}
 
 #[entry]
 fn main() -> ! {
@@ -11,7 +35,6 @@ fn main() -> ! {
     let mut system = peripherals.DPORT.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    // Disable the RTC and TIMG watchdog timers
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
     let timer_group0 = TimerGroup::new(
         peripherals.TIMG0,
@@ -25,11 +48,17 @@ fn main() -> ! {
         &mut system.peripheral_clock_control,
     );
     let mut wdt1 = timer_group1.wdt;
+
+    // Disable the RTC and TIMG watchdog timers
     rtc.rwdt.disable();
     wdt0.disable();
     wdt1.disable();
 
-    println!("Hello world!");
+    embassy::init(&clocks, timer_group0.timer0);
 
-    loop {}
+    let executor = EXECUTOR.init(Executor::new());
+    executor.run(|spawner| {
+        spawner.spawn(run1()).unwrap();
+        spawner.spawn(run2()).unwrap();
+    })
 }
