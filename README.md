3261d18f7d404a152c377222082727048deb9f23
steinuil <steenuil.owl@gmail.com>
2023-11-24 17:41:07 +0100

    recognize button presses and go into deep sleep

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+## esp32-hal documentation
+
+- https://docs.rs/esp32-hal/latest/
+- https://docs.rs/esp32/latest/
+
+## Data sheets and crates
+
 https://watchy.sqfmi.com/docs/hardware
 
 - Microcontroller ESP32-PICO-D4: esp-idf-hal ?
@@ -7,3 +14,15 @@ https://watchy.sqfmi.com/docs/hardware
   - https://github.com/ZinggJM/GxEPD/blob/master/src/GxGDEH0154D67/GxGDEH0154D67.cpp
 - Real time clock PCF8563: https://docs.rs/pcf8563/latest/pcf8563/
 - 3-axis accelerometer BMA423: https://docs.rs/bma423/latest/bma423/
+
+## esp toolchain on Nix
+
+- https://github.com/sdobz/rust-esp-nix (3 years old)
+- https://github.com/kate-shine/nix-esp32 (uses the docker image)
+- https://n8henrie.com/2023/09/compiling-rust-for-the-esp32-with-nix/ (doesn't support xtensa architectures)
+
+## TODO
+
+- BMA423: add states for initialized/fullpower/powersave like in the non-async crate
+- GDEH0154D67
+- PCF8563
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 #![no_std]
 #![no_main]
 #![feature(type_alias_impl_trait)]
+#![allow(clippy::empty_loop)]
 
-use embassy_executor::Executor;
+use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
+use esp32_hal::{
+    clock::ClockControl,
+    embassy,
+    peripherals::{Peripherals, RTC_CNTL},
+    prelude::*,
+    reset::SleepSource,
+    rtc_cntl::sleep::{Ext0WakeupSource, Ext1WakeupSource, WakeupLevel},
+    timer::TimerGroup,
+    Delay, IO,
+};
 use esp_backtrace as _;
 use esp_println::println;
-use hal::{
-    clock::ClockControl, embassy, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc,
-};
-use static_cell::StaticCell;
-
-static EXECUTOR: StaticCell<Executor> = StaticCell::new();
 
 #[embassy_executor::task]
 async fn run1() {
@@ -29,36 +34,104 @@ async fn run2() {
     }
 }
 
-#[entry]
-fn main() -> ! {
+const RTCIO_GPIO4_CHANNEL: u32 = 1 << 10;
+const RTCIO_GPIO25_CHANNEL: u32 = 1 << 6;
+const RTCIO_GPIO26_CHANNEL: u32 = 1 << 7;
+const RTCIO_GPIO35_CHANNEL: u32 = 1 << 5;
+
+#[repr(u8)]
+#[derive(Debug, Clone, Copy)]
+enum Button {
+    /// Bottom left button
+    Menu,
+    /// Top left button
+    Back,
+    /// Top right button
+    Up,
+    /// Bottom right button
+    Down,
+}
+
+fn get_ext1_wakeup_button(rtc_cntl: RTC_CNTL) -> Result<Button, u32> {
+    let wakeup_bits = rtc_cntl.ext_wakeup1_status.read().bits();
+
+    match wakeup_bits {
+        RTCIO_GPIO26_CHANNEL => Ok(Button::Menu),
+        RTCIO_GPIO25_CHANNEL => Ok(Button::Back),
+        RTCIO_GPIO35_CHANNEL => Ok(Button::Up),
+        RTCIO_GPIO4_CHANNEL => Ok(Button::Down),
+        _ => Err(wakeup_bits),
+    }
+}
+
+#[main]
+async fn main(_spawner: Spawner) {
     let peripherals = Peripherals::take();
-    let mut system = peripherals.DPORT.split();
+    let system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-    let timer_group0 = TimerGroup::new(
-        peripherals.TIMG0,
-        &clocks,
-        &mut system.peripheral_clock_control,
-    );
-    let mut wdt0 = timer_group0.wdt;
-    let timer_group1 = TimerGroup::new(
-        peripherals.TIMG1,
-        &clocks,
-        &mut system.peripheral_clock_control,
-    );
-    let mut wdt1 = timer_group1.wdt;
+    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
 
-    // Disable the RTC and TIMG watchdog timers
-    rtc.rwdt.disable();
-    wdt0.disable();
-    wdt1.disable();
+    let mut rtc = esp32_hal::Rtc::new(peripherals.RTC_CNTL);
+
+    let mut io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
 
     embassy::init(&clocks, timer_group0.timer0);
 
-    let executor = EXECUTOR.init(Executor::new());
-    executor.run(|spawner| {
-        spawner.spawn(run1()).unwrap();
-        spawner.spawn(run2()).unwrap();
-    })
+    let cause = esp32_hal::reset::get_wakeup_cause();
+
+    match cause {
+        // RTC alarm
+        SleepSource::Ext0 => {
+            println!("RTC alarm (display needs to be updated)");
+        }
+        // Button press
+        SleepSource::Ext1 => match get_ext1_wakeup_button(peripherals.RTC_CNTL) {
+            Ok(Button::Menu) => {
+                println!("Menu button pressed");
+            }
+            Ok(Button::Back) => {
+                println!("Down button pressed");
+            }
+            Ok(Button::Up) => {
+                println!("Up button pressed");
+            }
+            Ok(Button::Down) => {
+                println!("Down button pressed");
+            }
+            Err(wakeup_status) => {
+                println!("wakeup_status bitmask not recognized: {}", wakeup_status);
+            }
+        },
+        // Booted
+        SleepSource::Undefined => {
+            println!("Booted");
+        }
+        _ => {
+            println!("unknown wakeup cause: {:?}", cause);
+        }
+    }
+
+    let mut delay = Delay::new(&clocks);
+
+    rtc.sleep_deep(
+        &[
+            // should be low according to the C code
+            // &Ext0WakeupSource::new(&mut io.pins.gpio27, WakeupLevel::Low),
+            &Ext1WakeupSource::new(
+                &mut [
+                    // Menu button
+                    &mut io.pins.gpio26,
+                    // Back button
+                    &mut io.pins.gpio25,
+                    // Down button
+                    &mut io.pins.gpio4,
+                    // Up button
+                    &mut io.pins.gpio35,
+                ],
+                WakeupLevel::High,
+            ),
+        ],
+        &mut delay,
+    );
 }
