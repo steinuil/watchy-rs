ae5add576c60666968b850060bdfb76bf82d590d
steinuil <steenuil.owl@gmail.com>
2023-11-27 20:03:41 +0100

    WIP display

--- gdeh0154d67-async/src/lib.rs
+++ gdeh0154d67-async/src/lib.rs
@@ -1,1 +1,252 @@
 #![no_std]
+
+use core::convert::Infallible;
+
+use embedded_hal::digital::{InputPin, OutputPin};
+use embedded_hal_async::{
+    delay::DelayUs,
+    spi::{SpiBus, SpiDevice},
+};
+
+#[derive(Debug)]
+pub enum Error {}
+
+// impl<E: core::fmt::Display> core::fmt::Display for Error {
+//     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+//         match self {
+//             Error::Bus(e) => write!(f, "Bus error: {}", e),
+//         }
+//     }
+// }
+
+// impl<E> From<E> for Error {
+//     fn from(err: E) -> Self {
+//         Error::Bus(err)
+//     }
+// }
+
+mod command {
+    pub const SW_RESET: u8 = 0x12;
+    pub const DRIVER_OUTPUT_CONTROL: u8 = 0x01;
+    pub const DEEP_SLEEP_MODE: u8 = 0x10;
+    pub const DATA_ENTRY_MODE_SETTING: u8 = 0x11;
+    pub const TEMPERATURE_SENSOR_CONTROL: u8 = 0x18;
+    pub const MASTER_ACTIVATION: u8 = 0x20;
+    pub const DISPLAY_UPDATE_CONTROL_1: u8 = 0x21;
+    pub const DISPLAY_UPDATE_CONTROL_2: u8 = 0x22;
+    pub const WRITE_RAM_BW: u8 = 0x24;
+    pub const BORDER_WAVEFORM_CONTROL: u8 = 0x3c;
+    pub const SET_RAM_X_START_END_POSITION: u8 = 0x44;
+    pub const SET_RAM_Y_START_END_POSITION: u8 = 0x45;
+    pub const SET_RAM_X_ADDRESS_POSITION: u8 = 0x4e;
+    pub const SET_RAM_Y_ADDRESS_POSITION: u8 = 0x4f;
+}
+
+#[repr(u8)]
+#[derive(Debug, Clone, Copy)]
+pub enum TemperatureSensor {
+    External = 0x48,
+    Internal = 0x80,
+}
+
+#[repr(u8)]
+#[derive(Debug, Clone, Copy)]
+pub enum DeepSleepMode {
+    Normal = 0b00,
+    RetainRAM = 0b01,
+    ResetRAM = 0b11,
+}
+
+pub struct GDEH0154D67<SPI, DC, RES, Busy, Delay> {
+    spi: SPI,
+    dc: DC,
+    reset: RES,
+    busy: Busy,
+    delay: Delay,
+}
+impl<SPI, DC, RES, Busy, Delay> GDEH0154D67<SPI, DC, RES, Busy, Delay>
+where
+    SPI: SpiBus,
+    DC: OutputPin<Error = Infallible>,
+    RES: OutputPin<Error = Infallible>,
+    Busy: InputPin<Error = Infallible>,
+    Delay: DelayUs,
+{
+    pub fn new(
+        spi: SPI,
+        data_command_pin: DC,
+        reset_pin: RES,
+        busy_pin: Busy,
+        delay: Delay,
+    ) -> Self {
+        GDEH0154D67 {
+            spi,
+            dc: data_command_pin,
+            reset: reset_pin,
+            busy: busy_pin,
+            delay,
+        }
+    }
+
+    pub async fn initialize(&mut self) -> Result<(), Error> {
+        self.hardware_reset().await?;
+        self.software_reset().await?;
+        self.set_driver_output().await?;
+        self.set_data_entry_mode().await?;
+        self.set_ram_x_start_end_position(0, 200).await?;
+        self.set_ram_y_start_end_position(0, 200).await?;
+        self.set_border_waveform().await?;
+        self.set_temperature_sensor(TemperatureSensor::Internal)
+            .await?;
+        self.set_display_update_sequence(0xb1).await?;
+        self.master_activation().await?;
+        Ok(())
+    }
+
+    pub async fn draw(&mut self, data: &[u8]) -> Result<(), Error> {
+        self.write_bw_ram(data).await?;
+        self.set_display_update_sequence(0xc7).await?;
+        self.set_deep_sleep_mode(DeepSleepMode::RetainRAM).await?;
+        Ok(())
+    }
+
+    async fn hardware_reset(&mut self) -> Result<(), Error> {
+        self.reset.set_low().unwrap();
+        self.delay.delay_ms(10).await;
+        self.reset.set_high().unwrap();
+        self.delay.delay_ms(10).await;
+        Ok(())
+    }
+
+    async fn software_reset(&mut self) -> Result<(), Error> {
+        self.write_command(command::SW_RESET).await?;
+        // According to the SSD1681 spec
+        self.delay.delay_ms(10).await;
+        Ok(())
+    }
+
+    async fn set_driver_output(&mut self) -> Result<(), Error> {
+        self.write_command_data(command::DRIVER_OUTPUT_CONTROL, &[0xc7, 0b0, 0x00])
+            .await?;
+        Ok(())
+    }
+
+    async fn set_data_entry_mode(&mut self) -> Result<(), Error> {
+        self.write_command_data(command::DATA_ENTRY_MODE_SETTING, &[0b0_11])
+            .await?;
+        Ok(())
+    }
+
+    async fn set_ram_x_start_end_position(&mut self, x: u16, width: u16) -> Result<(), Error> {
+        self.write_command_data(
+            command::SET_RAM_X_START_END_POSITION,
+            &[(x / 8) as u8, ((x + width - 1) / 8) as u8],
+        )
+        .await?;
+        Ok(())
+    }
+
+    async fn set_ram_y_start_end_position(&mut self, y: u16, height: u16) -> Result<(), Error> {
+        self.write_command_data(
+            command::SET_RAM_Y_START_END_POSITION,
+            &[
+                (y % 0xFF) as u8,
+                (y / 0xFF) as u8,
+                ((y + height - 1) % 0xFF) as u8,
+                ((y + height - 1) / 0xFF) as u8,
+            ],
+        )
+        .await?;
+        Ok(())
+    }
+
+    async fn set_ram_x_address_position(&mut self, x: u16) -> Result<(), Error> {
+        self.write_command_data(command::SET_RAM_X_ADDRESS_POSITION, &[(x / 8) as u8])
+            .await?;
+        Ok(())
+    }
+
+    async fn set_ram_y_address_position(&mut self, y: u16) -> Result<(), Error> {
+        self.write_command_data(
+            command::SET_RAM_Y_ADDRESS_POSITION,
+            &[(y % 256) as u8, (y / 256) as u8],
+        )
+        .await?;
+        Ok(())
+    }
+
+    // TODO provide some parameters to control the border waveform
+    async fn set_border_waveform(&mut self) -> Result<(), Error> {
+        self.write_command_data(command::BORDER_WAVEFORM_CONTROL, &[0b101])
+            .await?;
+        Ok(())
+    }
+
+    async fn set_temperature_sensor(&mut self, sensor: TemperatureSensor) -> Result<(), Error> {
+        self.write_command_data(command::TEMPERATURE_SENSOR_CONTROL, &[sensor as u8])
+            .await?;
+        Ok(())
+    }
+
+    // TODO use bitmask or something for this
+    // 0xb1 before writing RAM
+    // 0xc7 to display
+    // 7 = enable clock signal
+    // 6 = enable analog
+    // 5 = Load temperature value
+    // 4 = load LUT with DISPLAY Mode 1
+    // 3 = load LUT or display with DISPLAY Mode 2
+    // 2 = display with DISPLAY Mode 1
+    // 1 = disable analog
+    // 0 = disable clock signal
+    async fn set_display_update_sequence(&mut self, sequence: u8) -> Result<(), Error> {
+        self.write_command_data(command::DISPLAY_UPDATE_CONTROL_2, &[sequence])
+            .await?;
+        Ok(())
+    }
+
+    async fn master_activation(&mut self) -> Result<(), Error> {
+        self.write_command(command::MASTER_ACTIVATION).await?;
+        self.busy_wait().await?;
+        Ok(())
+    }
+
+    async fn write_bw_ram(&mut self, data: &[u8]) -> Result<(), Error> {
+        self.write_command_data(command::WRITE_RAM_BW, data).await?;
+        Ok(())
+    }
+
+    async fn set_deep_sleep_mode(&mut self, mode: DeepSleepMode) -> Result<(), Error> {
+        self.write_command_data(command::DEEP_SLEEP_MODE, &[mode as u8])
+            .await?;
+        Ok(())
+    }
+
+    // async fn set_border_waveform(&mut self, )
+
+    async fn busy_wait(&mut self) -> Result<(), Error> {
+        while self.busy.is_high().unwrap() {
+            self.delay.delay_ms(10).await;
+        }
+
+        Ok(())
+    }
+
+    async fn write_command_data(&mut self, command: u8, data: &[u8]) -> Result<(), Error> {
+        self.write_command(command).await?;
+        self.write_data(data).await?;
+        Ok(())
+    }
+
+    async fn write_command(&mut self, command: u8) -> Result<(), Error> {
+        self.dc.set_low().unwrap();
+        self.spi.write(&[command]).await.unwrap();
+        Ok(())
+    }
+
+    async fn write_data(&mut self, data: &[u8]) -> Result<(), Error> {
+        self.dc.set_high().unwrap();
+        self.spi.write(data).await.unwrap();
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,12 @@ use esp32_hal::{
     clock::ClockControl,
     embassy,
     i2c::I2C,
+    pdma::{Dma, Spi3DmaChannel},
     peripherals::{Interrupt, Peripherals, RTC_CNTL},
     prelude::*,
     reset::SleepSource,
     rtc_cntl::sleep::{Ext0WakeupSource, Ext1WakeupSource, WakeupLevel},
+    spi::master::{prelude::*, Spi, SpiBusController, SpiBusDevice},
     timer::TimerGroup,
     Delay, Rtc, IO,
 };
@@ -50,6 +52,8 @@ enum Button {
 }
 
 fn get_ext1_wakeup_button(_rtc: &Rtc) -> Result<Button, u32> {
+    // TODO when esp32_hal lets you read the wakeup status, it'd be nice to use that
+    // instead of using unsafe.
     let wakeup_bits = unsafe { (*RTC_CNTL::PTR).ext_wakeup1_status.read() }.bits();
 
     match wakeup_bits {
@@ -74,8 +78,19 @@ async fn main(_spawner: Spawner) {
 
     let sda_pin = io.pins.gpio21;
     let scl_pin = io.pins.gpio22;
-    let vibration_motor_pin = io.pins.gpio13;
+
+    let sck_pin = io.pins.gpio18;
+    let mosi_pin = io.pins.gpio23;
+    let cs_pin = io.pins.gpio5;
+
+    let dc_pin = io.pins.gpio10;
+    let reset_pin = io.pins.gpio9;
+    let busy_pin = io.pins.gpio19;
+
+    let vibration_motor_pin = io.pins.gpio13.into_push_pull_output();
+
     let mut rtc_interrupt_pin = io.pins.gpio27;
+
     let mut button_bottom_left_pin = io.pins.gpio26;
     let mut button_top_left_pin = io.pins.gpio25;
     let mut button_top_right_pin = io.pins.gpio35;
@@ -92,12 +107,26 @@ async fn main(_spawner: Spawner) {
     )
     .unwrap();
 
-    let mut pcf8563 = pcf8563_async::PCF8563::new(pcf8563_async::SLAVE_ADDRESS, &mut i2c);
+    let dma = Dma::new(system.dma);
 
-    match pcf8563.read_datetime().await {
-        Ok(time) => println!("time: {}", time),
-        Err(e) => println!("error reading time: {:?}", e),
-    }
+    let mut tx_descr = [0u32; 8 * 3];
+    let mut rx_descr = [0u32; 8 * 3];
+
+    let spi = Spi::new_no_miso(
+        peripherals.SPI2,
+        sck_pin,
+        mosi_pin,
+        cs_pin,
+        20u32.MHz(),
+        esp32_hal::spi::SpiMode::Mode0,
+        &clocks,
+    )
+    .with_dma(dma.spi2channel.configure(
+        false,
+        &mut tx_descr,
+        &mut rx_descr,
+        esp32_hal::dma::DmaPriority::Priority0,
+    ));
 
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
 
@@ -122,6 +151,26 @@ async fn main(_spawner: Spawner) {
         SleepSource::Ext1 => match get_ext1_wakeup_button(&rtc) {
             Ok(Button::BottomLeft) => {
                 println!("Menu button pressed");
+
+                {
+                    let mut gdeh0154d67 = gdeh0154d67_async::GDEH0154D67::new(
+                        spi,
+                        dc_pin.into_push_pull_output(),
+                        reset_pin.into_push_pull_output(),
+                        busy_pin.into_pull_up_input(),
+                        embassy_time::Delay,
+                    );
+
+                    println!("display acquired");
+
+                    gdeh0154d67.initialize().await.unwrap();
+                    println!("display initialized");
+                    gdeh0154d67
+                        .draw(&[0xFF, 0xFF, 0xFF, 0xFF, 0xFF])
+                        .await
+                        .unwrap();
+                    println!("drawn");
+                }
             }
             Ok(Button::TopLeft) => {
                 println!("Back button pressed");
@@ -145,15 +194,24 @@ async fn main(_spawner: Spawner) {
         }
     }
 
-    let minute = pcf8563.read_time().await.unwrap().minute();
-    pcf8563
-        .set_alarm_interrupt(&pcf8563_async::AlarmConfig {
-            minute: Some(if minute >= 59 { 0 } else { minute + 1 }),
-            ..Default::default()
-        })
-        .await
-        .unwrap();
-    pcf8563.enable_alarm_interrupt().await.unwrap();
+    {
+        let mut pcf8563 = pcf8563_async::PCF8563::new(pcf8563_async::SLAVE_ADDRESS, &mut i2c);
+
+        match pcf8563.read_datetime().await {
+            Ok(time) => println!("time: {}", time),
+            Err(e) => println!("error reading time: {:?}", e),
+        }
+
+        let minute = pcf8563.read_time().await.unwrap().minute();
+        pcf8563
+            .set_alarm_interrupt(&pcf8563_async::AlarmConfig {
+                minute: Some(if minute >= 59 { 0 } else { minute + 1 }),
+                ..Default::default()
+            })
+            .await
+            .unwrap();
+        pcf8563.enable_alarm_interrupt().await.unwrap();
+    }
 
     let mut delay = Delay::new(&clocks);
 
